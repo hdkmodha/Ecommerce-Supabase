d4c8038325b53fccd81050b979a2ad2ec43b932c
Hardik Modha <hardik.modha27@gmail.com>
2026-04-20 13:57:03 +0530

    Initial Commit

---
+++ ECommereceApp/ECommereceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ECommereceAppApp.swift
+//  ECommereceApp
+//
+//  Created by Hardik Modha on 20/04/26.
+//
+
+import SwiftUI
+
+@main
+struct ECommereceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
